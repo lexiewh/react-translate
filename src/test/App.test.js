d800543e18c8e4fac95597b451d892e4731b93ee
Lexie White <lexwhite028@gmail.com>
2021-08-19 21:53:25 -0500

    all included changes

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// eslint-disable-next-line no-unused-vars
+import { toBeInTheDocument } from '@testing-library/jest-dom';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
